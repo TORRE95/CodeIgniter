6296a16e70a7d1d070b9d94116ebcd8a325670f0
Unknown <torrewz@gmail.com>
2018-01-06 17:33:14 -0600

    6 enero

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -16,35 +16,57 @@ class Users extends CI_Controller{
 		
 	}
 
-	public function insert(){
+	// public function insert(){
 
-		$username = "peter";
-		$password = "secret";
+	// 	$username = "peter";
+	// 	$password = "secret";
 
-		$this->user_model->create_users([
+	// 	$this->user_model->create_users([
 
-			'username' => $username,
-			'password' => $password
-		]);
+	// 		'username' => $username,
+	// 		'password' => $password
+	// 	]);
+	// }
+
+	// public function update(){
+
+	// 	$id = 3;
+	// 	$username = "William";
+	// 	$password = "not so secret";
+
+	// 	$this->user_model->update_users([
+
+	// 		'username' => $username,
+	// 		'password' => $password
+	// 	], $id);
+	// }
+
+	// public function delete(){
+
+	// 	$id = 3;
+	// 	$this->user_model->delete_users($id);
+	// }
+
+	public function login(){
+
+		/*Validación para form | reglas de validacion*/
+		$this->form_validation->set_rules('username','username', 'trim|required|min_length[3]');
+		$this->form_validation->set_rules('password','password', 'trim|required|min_length[3]');
+		$this->form_validation->set_rules('confirm_password','confirm_password', 'trim|required|min_length[3]|matches[password]');
+
+		if($this->form_validation->run() == FALSE){
+
+			$data = array(
+					'errors'=>validation_errors()
+
+					);
+			$this->session->set_flashdata($data);
+
+			redirect('home');
+		}
+
+		// echo $this->input->post('username'); //Remplaza a echo $_POST[];
 	}
 
-	public function update(){
-
-		$id = 3;
-		$username = "William";
-		$password = "not so secret";
-
-		$this->user_model->update_users([
-
-			'username' => $username,
-			'password' => $password
-		], $id);
-	}
-
-	public function delete(){
-
-		$id = 3;
-		$this->user_model->delete_users($id);
-	}
 }
 ?>
--- application/views/users/login_view.php
+++ application/views/users/login_view.php
@@ -1,6 +1,58 @@
 <h2>Login Form</h2>
 
 <?php 
-	
-	echo form_open('users/login_view', $attributes);
+	if($this->session->flashdata('errors')): echo $this->session->flashdata('errors'); endif;
+	$attributes = array('id'=>'login_form', 'class'=>'form_horizontal');
+	echo form_open('users/login', $attributes);
+?>
+<div class="form-group">
+	<?php
+	/*$data se usa para envíar los atributos de una etiqueta form*/
+	$data = array(
+			'class'=> 'form-control',
+			'name'=> 'username',
+			'placeholder'=> 'Enter Username',);
+
+		echo form_label('Username'); //Se crea una etiqueta label
+		echo form_input($data); //Se envían los atributos del form
+	?>
+</div>
+<div class="form-group">
+	<?php
+	/*$data se usa para envíar los atributos de una etiqueta form*/
+	$data = array(
+			'class'=> 'form-control',
+			'name'=> 'password',
+			'placeholder'=> 'Enter Password',);
+
+		echo form_label('Password'); //Se crea una etiqueta label
+		echo form_password($data); //Se envían los atributos del form
+	?>
+</div>
+<div class="form-group">
+	<?php
+	/*$data se usa para envíar los atributos de una etiqueta form*/
+	$data = array(
+			'class'=> 'form-control',
+			'name'=> 'confirm_password',
+			'placeholder'=> 'Confirm Password',);
+
+		echo form_label('Confirm Password'); //Se crea una etiqueta label
+		echo form_password($data); //Se envían los atributos del form
+	?>
+</div>
+<div class="form-group">
+	<?php
+	/*$data se usa para envíar los atributos de una etiqueta form*/
+	$data = array(
+			'class'=> 'btn btn-primary', //Clase para botones
+			'name'=> 'submit',
+			'value'=> 'Login',);
+
+		
+		echo form_submit($data);
+	?>
+</div>
+<?php
+	echo form_close();
 ?>
